c1b9490d9419e16489ae2df7f301497526b0e314
Enrico Degregori <degregori@dkrz.de>
2024-02-22 17:13:36 +0100

    add standard mergsort

--- src/mergesort.h
+++ src/mergesort.h
@@ -1,2 +1,3 @@
+void mergeSort(int *arr, int l, int r);
 void mergeSort_with_idx(int *arr, int *arr_idx, int l, int r);
 void mergeSort_with_idx2(int *arr, int *arr_idx1, int *arr_idx2, int l, int r);
