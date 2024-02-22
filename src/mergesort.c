fed49187b59e4b60538ac0a8e4615cb53b4b4d39
Enrico Degregori <degregori@dkrz.de>
2024-02-22 15:26:23 +0100

    extend mergesort to sort 2 idx arrays accordingly to sorted arr

--- src/mergesort.c
+++ src/mergesort.c
@@ -6,7 +6,7 @@
 // Merges two subarrays of arr[].
 // First subarray is arr[l..m]
 // Second subarray is arr[m+1..r]
-void merge(int *arr, int *arr_idx, int l, int m, int r)
+void merge_with_idx(int *arr, int *arr_idx, int l, int m, int r)
 {
     int i, j, k;
     int n1 = m - l + 1;
@@ -66,15 +66,98 @@ void merge(int *arr, int *arr_idx, int l, int m, int r)
  
 // l is for left index and r is right index of the
 // sub-array of arr to be sorted
-void mergeSort(int *arr, int *arr_idx, int l, int r)
+void mergeSort_with_idx(int *arr, int *arr_idx, int l, int r)
 {
     if (l < r) {
         int m = l + (r - l) / 2;
  
         // Sort first and second halves
-        mergeSort(arr, arr_idx, l, m);
-        mergeSort(arr, arr_idx, m + 1, r);
+        mergeSort_with_idx(arr, arr_idx, l, m);
+        mergeSort_with_idx(arr, arr_idx, m + 1, r);
  
-        merge(arr, arr_idx, l, m, r);
+        merge_with_idx(arr, arr_idx, l, m, r);
+    }
+}
+
+void merge_with_idx2(int *arr, int *arr_idx1, int *arr_idx2, int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+ 
+    // Create temp arrays
+    int L[n1], R[n2];
+    int L_idx1[n1], R_idx1[n2];
+    int L_idx2[n1], R_idx2[n2];
+ 
+    // Copy data to temp arrays L[] and R[]
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    for (i = 0; i < n1; i++)
+        L_idx1[i] = arr_idx1[l + i];
+    for (j = 0; j < n2; j++)
+        R_idx1[j] = arr_idx1[m + 1 + j];
+
+    for (i = 0; i < n1; i++)
+        L_idx2[i] = arr_idx2[l + i];
+    for (j = 0; j < n2; j++)
+        R_idx2[j] = arr_idx2[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r
+    i = 0;
+    j = 0;
+    k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            arr_idx1[k] = L_idx1[i];
+            arr_idx2[k] = L_idx2[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            arr_idx1[k] = R_idx1[j];
+            arr_idx2[k] = R_idx2[j];
+            j++;
+        }
+        k++;
+    }
+ 
+    // Copy the remaining elements of L[],
+    // if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        arr_idx1[k] = L_idx1[i];
+        arr_idx2[k] = L_idx2[i];
+        i++;
+        k++;
+    }
+ 
+    // Copy the remaining elements of R[],
+    // if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        arr_idx1[k] = R_idx1[j];
+        arr_idx2[k] = R_idx2[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is right index of the
+// sub-array of arr to be sorted
+void mergeSort_with_idx2(int *arr, int *arr_idx1, int *arr_idx2, int l, int r)
+{
+    if (l < r) {
+        int m = l + (r - l) / 2;
+ 
+        // Sort first and second halves
+        mergeSort_with_idx2(arr, arr_idx1, arr_idx2, l, m);
+        mergeSort_with_idx2(arr, arr_idx1, arr_idx2, m + 1, r);
+ 
+        merge_with_idx2(arr, arr_idx1, arr_idx2, l, m, r);
     }
 }
--- src/mergesort.h
+++ src/mergesort.h
@@ -1,1 +1,2 @@
-void mergeSort(int *arr, int *arr_idx, int l, int r);
+void mergeSort_with_idx(int *arr, int *arr_idx, int l, int r);
+void mergeSort_with_idx2(int *arr, int *arr_idx1, int *arr_idx2, int l, int r);
