1631780ae17d214ac24e73358c04a1ce3c3be590
Enrico Degregori <degregori@dkrz.de>
2024-03-24 23:34:31 +0100

    add function to check error returned from MPI

---
+++ src/check.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+
+#include "check.h"
+
+void check_mpi(int error_code) {
+    int rank;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    char error_string[MPI_MAX_ERROR_STRING];
+    int length_of_error_string, error_class;
+
+    if (error_code != MPI_SUCCESS) {
+        MPI_Comm_rank(comm, &rank);
+        MPI_Error_class(error_code, &error_class);
+        MPI_Error_string(error_class, error_string, &length_of_error_string);
+        fprintf(stderr, "%3d: %s\n", rank, error_string);
+        MPI_Error_string(error_code, error_string, &length_of_error_string);
+        fprintf(stderr, "%3d: %s\n", rank, error_string);
+        MPI_Abort(comm, error_code);
+    }
+
+}
---
+++ src/check.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <mpi.h>
+
+void check_mpi(int error_code);
