bef61d18cdc446a9b7f33b5d28ef657ef008c9c3
Enrico Degregori <degregori@dkrz.de>
2024-04-17 22:26:50 +0200

    python bindings: adapt test script to latest development

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -1,1 +1,18 @@
 import distdir
+import numpy as np
+from mpi4py import MPI
+
+
+size = MPI.COMM_WORLD.Get_size()
+rank = MPI.COMM_WORLD.Get_rank()
+
+array = [1, 2, 3, 4]
+
+if rank == 0:
+	src_idxlist = distdir.idxlist(array)
+	dst_idxlist = distdir.idxlist()
+else:
+	src_idxlist = distdir.idxlist()
+	dst_idxlist = distdir.idxlist(array)
+
+map = distdir.map(src_idxlist, dst_idxlist, MPI.COMM_WORLD)
