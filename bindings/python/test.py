e6eba2827a47c36300b8535f7004c972ae60344a
Enrico Degregori <degregori@dkrz.de>
2024-04-20 13:53:16 +0200

    python bindings: update test
    complete 1D trivial test using distdir in python

--- bindings/python/test.py
+++ bindings/python/test.py
@@ -11,8 +11,24 @@ array = [1, 2, 3, 4]
 if rank == 0:
 	src_idxlist = distdir.idxlist(array)
 	dst_idxlist = distdir.idxlist()
+	a = np.array([1,4,3,2], dtype=np.double)
 else:
 	src_idxlist = distdir.idxlist()
 	dst_idxlist = distdir.idxlist(array)
+	a = np.zeros(shape=(4), dtype=np.double)
+
+if rank == 0:
+	print('rank '+str(rank)+': '+str(a))
+else:
+	print('rank '+str(rank)+': '+str(a))
 
 map = distdir.map(src_idxlist, dst_idxlist, MPI.COMM_WORLD)
+
+exchanger = distdir.exchange()
+
+exchanger.go(map, a, a)
+
+if rank == 0:
+	print('rank '+str(rank)+': '+str(a))
+else:
+	print('rank '+str(rank)+': '+str(a))
