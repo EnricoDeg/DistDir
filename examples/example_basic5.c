9131de6f0460b4fead2e2ce8db4f8b34164ecaf8
Enrico Degregori <degregori@dkrz.de>
2024-04-28 01:58:28 +0200

    example5: add settings calls

--- examples/example_basic5.c
+++ examples/example_basic5.c
@@ -69,6 +69,8 @@
  */
 int example_basic5() {
 
+	distdir_initialize();
+
 	int world_rank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 	int world_size;
@@ -127,16 +129,15 @@ int example_basic5() {
 	delete_idxlist(p_dst_idxlist);
 	delete_map(p_map);
 
+	distdir_finalize();
+
 	return 0;
 }
 
 int main () {
 
-	MPI_Init(NULL,NULL);
-
 	int err = example_basic5();
 	if (err != 0) return err;
 
-	MPI_Finalize();
 	return 0;
 }
