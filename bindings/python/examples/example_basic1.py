5d1f72811a3f1a2394be4dbf96a0c3bc674b4bcf
Enrico Degregori <degregori@dkrz.de>
2024-05-04 16:50:54 +0200

    python: replicate example_basic1 in a python script

---
+++ bindings/python/examples/example_basic1.py
@@ -0,0 +1,38 @@
+import pydistdir
+import numpy as np
+from mpi4py import MPI
+
+dd = pydistdir.distdir()
+
+rank = MPI.COMM_WORLD.Get_rank()
+size = MPI.COMM_WORLD.Get_size()
+
+if size != 4:
+  raise Exception("Sorry, number of processes must be 4")
+
+# dummy way but easy to understand for the users
+if rank < 2:
+	if rank == 0:
+		src_idxlist = pydistdir.idxlist([0, 1, 4, 5, 8, 9, 12, 13])
+		data = np.array([0, 1, 2, 3, 4, 5, 6, 7], dtype = np.double)
+	else:
+		src_idxlist = pydistdir.idxlist([2, 3, 6, 7, 10, 11, 14, 15])
+		data = np.array([8, 9, 10, 11, 12, 13, 14, 15], dtype = np.double)
+	dst_idxlist = pydistdir.idxlist()
+	a = np.array([1,4,3,2], dtype=np.double)
+else:
+	if rank == 2:
+		dst_idxlist = pydistdir.idxlist([0, 1, 2, 3, 4, 5, 6, 7])
+		data = np.zeros(shape=(8), dtype = np.double)
+	else:
+		dst_idxlist = pydistdir.idxlist([8, 9, 10, 11, 12, 13, 14, 15])
+		data = np.zeros(shape=(8), dtype = np.double)
+	src_idxlist = pydistdir.idxlist()
+
+map = pydistdir.map(src_idxlist, dst_idxlist, -1, MPI.COMM_WORLD)
+
+exchanger = pydistdir.exchanger(map)
+
+exchanger.go(data, data)
+
+print('rank '+str(rank)+': '+str(data))
