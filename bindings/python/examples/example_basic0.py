de81955c342f76f8a31cfbc07fa951e017e58458
Enrico Degregori <degregori@dkrz.de>
2024-05-04 17:03:58 +0200

    python: clean up example_basic0 script

--- bindings/python/examples/example_basic0.py
+++ bindings/python/examples/example_basic0.py
@@ -9,21 +9,19 @@ dd.verbose(pydistdir.pydistdir_verbose.verbose_true)
 size = MPI.COMM_WORLD.Get_size()
 rank = MPI.COMM_WORLD.Get_rank()
 
-array = [1, 2, 3, 4]
+if size != 2:
+  raise Exception("Sorry, number of processes must be 2")
 
 if rank == 0:
-	src_idxlist = pydistdir.idxlist(array)
+	src_idxlist = pydistdir.idxlist([1, 2, 3, 4])
 	dst_idxlist = pydistdir.idxlist()
 	a = np.array([1,4,3,2], dtype=np.double)
 else:
 	src_idxlist = pydistdir.idxlist()
-	dst_idxlist = pydistdir.idxlist(array)
+	dst_idxlist = pydistdir.idxlist([1, 2, 3, 4])
 	a = np.zeros(shape=(4), dtype=np.double)
 
-if rank == 0:
-	print('rank '+str(rank)+': '+str(a))
-else:
-	print('rank '+str(rank)+': '+str(a))
+print('rank '+str(rank)+': '+str(a))
 
 map = pydistdir.map(src_idxlist=src_idxlist, dst_idxlist=dst_idxlist, comm=MPI.COMM_WORLD)
 
@@ -31,7 +29,4 @@ exchanger = pydistdir.exchanger(map)
 
 exchanger.go(a, a)
 
-if rank == 0:
-	print('rank '+str(rank)+': '+str(a))
-else:
-	print('rank '+str(rank)+': '+str(a))
+print('rank '+str(rank)+': '+str(a))
