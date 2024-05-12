4467cfb8799bb84db416c47e852945f4d818721b
EnricoDeg <73224202+EnricoDeg@users.noreply.github.com>
2024-05-12 12:12:48 +0000

    Deploying to gh-pages from @ EnricoDeg/DistDir@77737319d31b5a391bebea0dfe7974470f95eded 🚀

--- search/all_9.js
+++ search/all_9.js
@@ -17,7 +17,7 @@ var searchData=
   ['bucket_5fidxlist_5fprocs_30',['bucket_idxlist_procs',['../group__backend.html#ga8c4869ada07df247bda1434bb0c9aa51',1,'backend.h']]],
   ['bucket_5ftests_31',['Bucket_tests',['../group__bucket__tests.html',1,'']]],
   ['buffer_32',['buffer',['../structt__exchange.html#a60e820ca79ea02459cf044e02d1d0ec8',1,'t_exchange']]],
-  ['buffer_5fidxlist_33',['buffer_idxlist',['../structt__map__exch.html#a20914633a3aeeaa5d05f752b4203c8b8',1,'t_map_exch::buffer_idxlist()'],['../structt__exchange.html#ac88bdb48e1bf742f4c9f2348e36bdb13',1,'t_exchange::buffer_idxlist()']]],
+  ['buffer_5fidxlist_33',['buffer_idxlist',['../structt__exchange.html#ac88bdb48e1bf742f4c9f2348e36bdb13',1,'t_exchange::buffer_idxlist()'],['../structt__map__exch.html#a20914633a3aeeaa5d05f752b4203c8b8',1,'t_map_exch::buffer_idxlist()']]],
   ['buffer_5fidxlist_5fgpu_34',['buffer_idxlist_gpu',['../structt__map__exch.html#a29e028963cce644ce8fac04bae477c26',1,'t_map_exch']]],
   ['buffer_5foffset_35',['buffer_offset',['../structt__map__exch.html#a5494f9773927c57392f34c4ca98e6f57',1,'t_map_exch']]],
   ['buffer_5fsize_36',['buffer_size',['../structt__exchange.html#a633fb9ddf76352b4356f128d34e1ccb8',1,'t_exchange::buffer_size()'],['../structt__map__exch.html#aec7a3e54edf3e5aad1ae316e367ba4e5',1,'t_map_exch::buffer_size()']]]
