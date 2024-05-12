72b68a01b27dffb5a949af814d3906162aa0efe7
Enrico Degregori <degregori@dkrz.de>
2024-05-12 12:42:12 +0200

    doxygen: documentation of backend_mpi functions

--- src/backend_mpi.h
+++ src/backend_mpi.h
@@ -36,33 +36,168 @@
 
 #include "mpi.h"
 
+/**
+ * @brief Lightweight wrapper around MPI_Isend for integer buffers.
+ *  
+ * @param[in]  buffer   buffer to be sent
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  dest     rank to send the message to
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] request  MPI request for non blocking messages
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_isend_int(int *buffer, int count, MPI_Datatype datatype, int dest, int tag,
                            MPI_Comm comm, MPI_Request *request, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Isend for float buffers.
+ *  
+ * @param[in]  buffer   buffer to be sent
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  dest     rank to send the message to
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] request  MPI request for non blocking messages
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_isend_float(float *buffer, int count, MPI_Datatype datatype, int dest, int tag,
                              MPI_Comm comm, MPI_Request *request, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Isend for double buffers.
+ *  
+ * @param[in]  buffer   buffer to be sent
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  dest     rank to send the message to
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] request  MPI request for non blocking messages
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_isend_double(double *buffer, int count, MPI_Datatype datatype, int dest, int tag,
                               MPI_Comm comm, MPI_Request *request, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Irecv for integer buffers.
+ *  
+ * @param[in]  buffer   buffer to be received
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  source     rank to receive the message from
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] request  MPI request for non blocking messages
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_irecv_int(int *buffer, int count, MPI_Datatype datatype, int source, int tag,
                            MPI_Comm comm, MPI_Request *request, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Irecv for float buffers.
+ *  
+ * @param[in]  buffer   buffer to be received
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  source     rank to receive the message from
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] request  MPI request for non blocking messages
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_irecv_float(float *buffer, int count, MPI_Datatype datatype, int source, int tag,
                              MPI_Comm comm, MPI_Request *request, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Irecv for double buffers.
+ *  
+ * @param[in]  buffer   buffer to be received
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  source     rank to receive the message from
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] request  MPI request for non blocking messages
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_irecv_double(double *buffer, int count, MPI_Datatype datatype, int source, int tag,
                               MPI_Comm comm, MPI_Request *request, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Recv for integer buffers.
+ *  
+ * @param[in]  buffer   buffer to be received
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  source     rank to receive the message from
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] status   message status
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_recv_int(int *buffer, int count, MPI_Datatype datatype, int source, int tag,
                           MPI_Comm comm, MPI_Status *status, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Recv for float buffers.
+ *  
+ * @param[in]  buffer   buffer to be received
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  source     rank to receive the message from
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] status   message status
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_recv_float(float *buffer, int count, MPI_Datatype datatype, int source, int tag,
                             MPI_Comm comm, MPI_Status *status, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Recv for double buffers.
+ *  
+ * @param[in]  buffer   buffer to be received
+ * @param[in]  count    size of the message
+ * @param[in]  datatype data type of buffer
+ * @param[in]  source     rank to receive the message from
+ * @param[in]  tag      message tag
+ * @param[in]  comm     MPI communicator
+ * @param[out] status   message status
+ * @param[in]  offset   buffer offset
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_recv_double(double *buffer, int count, MPI_Datatype datatype, int source, int tag,
                              MPI_Comm comm, MPI_Status *status, int offset);
 
+/**
+ * @brief Lightweight wrapper around MPI_Waitall.
+ *  
+ * @param[in]  count    number of non blocking messages
+ * @param[in]  requests array of MPI request
+ * @param[out] statuses array of message statuses
+ * 
+ * @ingroup backend_mpi
+ */
 void mpi_wrapper_waitall(int count, MPI_Request *requests, MPI_Status *statuses);
 
 #endif
