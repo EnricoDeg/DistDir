779d042e036a5f76479a039bb9d2a533c87501bd
EnricoDeg <73224202+EnricoDeg@users.noreply.github.com>
2024-05-24 22:32:40 +0000

    Deploying to gh-pages from @ EnricoDeg/DistDir@417f8f4b4dae609d8aa1a433add0f911ce6331f9 🚀

--- search/all_b.js
+++ search/all_b.js
@@ -7,7 +7,7 @@ var searchData=
   ['delete_5fidxlist_49',['delete_idxlist',['../group__idxlist.html#gab2d74f9dae2dcd08ba12fc8bf2336312',1,'idxlist.h']]],
   ['delete_5fmap_50',['delete_map',['../group__map.html#ga4c059dac87d848393a3176692302b19e',1,'map.h']]],
   ['delete_5fmpi_5fexchanger_51',['delete_mpi_exchanger',['../group__backend__mpi.html#ga467982d31394d3bd1ccadd2d6ee00475',1,'backend_mpi.h']]],
-  ['delete_5fvtable_5fcpu_52',['delete_vtable_cpu',['../group__backend__cpu.html#ga16fb0fa687c80e60f40a5f97001d987d',1,'backend_cpu.h']]],
+  ['delete_5fvtable_52',['delete_vtable',['../group__backend__cpu.html#ga9cd29fa5e8a11e5a4a691f0e30ee0d99',1,'backend_cpu.h']]],
   ['distdir_2eh_53',['distdir.h',['../distdir_8h.html',1,'']]],
   ['distdir_5fexchanger_54',['distdir_exchanger',['../setting_8h.html#a949370a7049e027f9bb7dda8451a1ef5',1,'setting.h']]],
   ['distdir_5ffinalize_55',['distdir_finalize',['../setting_8h.html#aec38b53906f3988b782b517f241fc658',1,'setting.h']]],
--- search/functions_3.js
+++ search/functions_3.js
@@ -6,7 +6,7 @@ var searchData=
   ['delete_5fidxlist_276',['delete_idxlist',['../group__idxlist.html#gab2d74f9dae2dcd08ba12fc8bf2336312',1,'idxlist.h']]],
   ['delete_5fmap_277',['delete_map',['../group__map.html#ga4c059dac87d848393a3176692302b19e',1,'map.h']]],
   ['delete_5fmpi_5fexchanger_278',['delete_mpi_exchanger',['../group__backend__mpi.html#ga467982d31394d3bd1ccadd2d6ee00475',1,'backend_mpi.h']]],
-  ['delete_5fvtable_5fcpu_279',['delete_vtable_cpu',['../group__backend__cpu.html#ga16fb0fa687c80e60f40a5f97001d987d',1,'backend_cpu.h']]],
+  ['delete_5fvtable_279',['delete_vtable',['../group__backend__cpu.html#ga9cd29fa5e8a11e5a4a691f0e30ee0d99',1,'backend_cpu.h']]],
   ['distdir_5ffinalize_280',['distdir_finalize',['../setting_8h.html#aec38b53906f3988b782b517f241fc658',1,'setting.h']]],
   ['distdir_5finitialize_281',['distdir_initialize',['../setting_8h.html#a6edc2c7cfa96bc4f790c2c857b69b787',1,'setting.h']]]
 ];
