685e32bb29ba62b7ab082cc4a63bc639d864f2a4
Enrico Degregori <degregori@dkrz.de>
2024-05-26 14:39:13 +0200

    Fortran bindings: set up cmake and main files
     - create file to handles C structures in Fortran (extension of C part)
     - create fortran module
     - create first fortran example
    
    The fortran code is currently inside the distdir library if Fortran
    bindings are enabled

---
+++ bindings/Fortran/distdir_mod.f90
@@ -0,0 +1,78 @@
+!
+! @file distdir_mod.f90
+!
+! @copyright Copyright (C) 2024 Enrico Degregori <enrico.degregori@gmail.com>
+!
+! @author Enrico Degregori <enrico.degregori@gmail.com>
+!
+! Redistribution and use in source and binary forms, with or without
+! modification, are permitted provided that the following conditions are met:
+!
+! 1. Redistributions of source code must retain the above copyright notice, this
+!    list of conditions and the following disclaimer.
+!
+! 2. Redistributions in binary form must reproduce the above copyright notice,
+!    this list of conditions and the following disclaimer in the documentation
+!    and/or other materials provided with the distribution.
+!
+! 3. Neither the name of the copyright holder nor the names of its
+!    contributors may be used to endorse or promote products derived from
+!    this software without specific prior written permission.
+! 
+! THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+! AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+! IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+! DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+! FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+! DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+! SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+! CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+! OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+! OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+!
+
+MODULE distdir_mod
+
+	USE, INTRINSIC :: ISO_C_BINDING, ONLY: c_ptr, c_int, c_null_ptr
+	IMPLICIT NONE
+
+	! note: this type must not be extended to contain any other
+	! components, its memory pattern has to match void * exactly, which
+	! it does because of C constraints
+	TYPE, BIND(C), PUBLIC :: t_idxlist
+#ifndef __G95__
+		PRIVATE
+#endif
+		TYPE(c_ptr) :: cptr = c_null_ptr
+	END TYPE t_idxlist
+
+	INTERFACE
+
+		! this function must not be implemented in Fortran because
+		! PGI 11.x chokes on that
+		FUNCTION t_idxlist_f2c(idxlist) BIND(c, name='t_idxlist_f2c') RESULT(p)
+			IMPORT :: c_ptr, t_idxlist
+			IMPLICIT NONE
+			TYPE(t_idxlist), INTENT(IN) :: idxlist
+			TYPE(c_ptr)                 :: p
+		END FUNCTION t_idxlist_f2c
+
+		FUNCTION new_idxlist_c(list, num_indices) &
+		                        BIND(C, name='new_idxlist') RESULT(res_ptr)
+			IMPORT :: c_ptr, c_int
+			IMPLICIT NONE
+			INTEGER(c_int),        INTENT(IN) :: list(*)
+			INTEGER(c_int), VALUE, INTENT(IN) :: num_indices
+			TYPE(c_ptr)                       :: res_ptr
+		END FUNCTION new_idxlist_c
+
+		SUBROUTINE delete_idxlist_c(idxlist) BIND(C, name='delete_idxlist')
+			IMPORT :: t_idxlist
+			IMPLICIT NONE
+			TYPE(t_idxlist), INTENT(in) :: idxlist
+		END SUBROUTINE delete_idxlist_c
+
+	END INTERFACE
+
+
+END MODULE distdir_mod
---
+++ src/utils/distdir_f2c.c
@@ -0,0 +1,78 @@
+/*
+ * @file distdir_f2c.c
+ *
+ * @copyright Copyright (C) 2024 Enrico Degregori <enrico.degregori@gmail.com>
+ *
+ * @author Enrico Degregori <enrico.degregori@gmail.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <mpi.h>
+
+#include "src/setup/setting.h"
+#include "src/setup/group.h"
+#include "src/core/indices/idxlist.h"
+#include "src/core/algorithm/map.h"
+#include "src/core/exchange/exchange.h"
+
+struct t_idxlist_f {
+	t_idxlist *cptr;
+};
+
+struct t_map_f {
+	t_map *cptr;
+};
+
+struct t_exchanger_f {
+	t_exchanger *cptr;
+};
+
+t_idxlist * t_idxlist_f2c(struct t_idxlist_f *p)
+{
+	return p->cptr;
+}
+
+t_map * t_xmap_f2c(struct t_map_f *p)
+{
+	return p->cptr;
+}
+
+t_exchanger * t_exchanger_f2c(struct t_exchanger_f *p)
+{
+	return p->cptr;
+}
+
+
+
+t_map * new_map_f(struct t_idxlist_f *src_idxlist_f,
+                  struct t_idxlist_f *dst_idxlist_f,
+                  int stride,
+                  MPI_Fint comm_f)
+{
+	MPI_Comm comm_c = MPI_Comm_f2c(comm_f);
+	return new_map(src_idxlist_f->cptr, dst_idxlist_f->cptr, stride, comm_c);
+}
