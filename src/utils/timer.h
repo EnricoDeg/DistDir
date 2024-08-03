c492fb7192e66b893ac871a57a1f9f5b155b2a88
Enrico Degregori <degregori@dkrz.de>
2024-08-03 23:24:07 +0200

    timer: add documentation to the module

--- src/utils/timer.h
+++ src/utils/timer.h
@@ -38,32 +38,107 @@
 
 #define STRING_MAX 1024
 
+/** @struct t_timer_data
+ * 
+ *  @brief The structure contains information about a single timer
+ * 
+ */
 struct t_timer_data {
 
+	/** @brief timer ID */
 	int id;
+	/** @brief timer name */
 	char* name;
+	/** @brief timer total time */
 	double total_time;
+	/** @brief timer start time */
 	double start_time;
 };
 typedef struct t_timer_data t_timer_data;
 
+/** @struct t_list_node
+ * 
+ *  @brief The structure contains a linked list single node for the timer module
+ * 
+ */
 struct t_list_node
 {
+
+	/** @brief pointer to t_timer_data holding single timer information */
 	t_timer_data *data;
+	/** @brief pointer to next node of the linked list */
 	struct t_list_node *next;
 };
 typedef struct t_list_node t_list_node;
 
+/**
+ * @brief Create a new timer
+ * 
+ * @details A timer is created and added to the internal linked list,
+ *          then the timer ID is returned.
+ *          If the timer already exists, its ID is returned.
+ * 
+ * @param[in] timer_name timer name string
+ * 
+ * @return timer ID
+ * 
+ * @ingroup timer
+ */
 int new_timer(const char * timer_name) ;
 
+/**
+ * @brief Start timer based on ID.
+ * 
+ * @details The timer is started using internally MPI_Wtime()
+ * 
+ * @param[in] timer_name timer name string
+ * 
+ * @ingroup timer
+ */
 void timer_start(int timer_id) ;
 
+/**
+ * @brief Stop timer based on ID.
+ * 
+ * @details The timer is stopped using internally MPI_Wtime() and 
+ *          adding the time to the total timer time.
+ *          If timer_start() was not called before, it fails.
+ * 
+ * @param[in] timer_name timer name string
+ * 
+ * @ingroup timer
+ */
 void timer_stop(int timer_id) ;
 
+/**
+ * @brief Create reports of all timers.
+ * 
+ * @details The reports are stored in a specific directory and 
+ *          a report is created by each MPI rank.
+ *          A summary report is also created with min, max and mean 
+ *          timers' times.
+ * 
+ * @ingroup timer
+ */
 void timers_report() ;
 
+/**
+ * @brief Reset all the timers.
+ * 
+ * @details The total time of all the timers is reset to zero.
+ * 
+ * @ingroup timer
+ */
 void timers_reset() ;
 
+/**
+ * @brief Free timers memory.
+ * 
+ * @details Free each timer memory and the memory related to the linked 
+ *          list.
+ * 
+ * @ingroup timer
+ */
 void delete_timers() ;
 
 #endif
